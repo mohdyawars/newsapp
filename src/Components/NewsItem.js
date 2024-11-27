f48e605bb8f9ae6f676c91d925225c61e31360cf
mohdyawars <mohdyawars@gmail.com>
2024-11-27 19:34:42 +0530

    API fetching complete

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,6 +2,32 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>This is a news item 2</div>;
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrl
+                ? "https://image.cnbcfm.com/api/v1/image/108068011-1732601507534-gettyimages-2155830976-SCOTT_BESSENT_INTERVIEW.jpeg?v=1732602177&w=1920&h=1080"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
