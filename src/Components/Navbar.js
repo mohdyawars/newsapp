d62a7bb59cb4393a88decd67cca983facc9f5f50
Yawar <54320537+mohdyawars@users.noreply.github.com>
2024-12-03 19:15:46 +0530

    added categories in navbar (#4)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,13 +26,49 @@ export default class NavBar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                  <a className="nav-link" aria-current="page" href="/">
                     Home
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    About
                   </a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/about">
-                    About
+                    business
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    entertainment
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    general
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    health
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    science
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    sports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    technology
                   </a>
                 </li>
               </ul>
