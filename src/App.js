f0822a2d19e2a59d356f393b262df8ac7eefdcd4
mohdyawars <mohdyawars@gmail.com>
2025-01-29 20:16:33 +0530

    news by categories done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,109 @@
-import "./App.css";
+import './App.css';
 
-import React, { Component } from "react";
-import NavBar from "./components/Navbar";
-import News from "./components/News";
+import React, { Component } from 'react';
+import NavBar from './components/Navbar';
+import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <News pageSize={5} country="us" category="sports" />
-      </div>
-    );
-  }
+    pageSize = 5;
+    render() {
+        return (
+            <div>
+                <Router>
+                    <NavBar />
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={
+                                <News
+                                    key='general'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='general'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/business'
+                            element={
+                                <News
+                                    key='business'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='business'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/entertainment'
+                            element={
+                                <News
+                                    key='entertainment'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='entertainment'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/general'
+                            element={
+                                <News
+                                    key='general'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='general'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/health'
+                            element={
+                                <News
+                                    key='health'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='health'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/science'
+                            element={
+                                <News
+                                    key='science'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='science'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/sports'
+                            element={
+                                <News
+                                    key='sports'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='sports'
+                                />
+                            }
+                        />
+                        <Route
+                            path='/technology'
+                            element={
+                                <News
+                                    key='technology'
+                                    pageSize={this.pageSize}
+                                    country='us'
+                                    category='technology'
+                                />
+                            }
+                        />
+                    </Routes>
+                </Router>
+            </div>
+        );
+    }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,34 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img
-            src={
-              !imageUrl
-                ? "https://image.cnbcfm.com/api/v1/image/108068011-1732601507534-gettyimages-2155830976-SCOTT_BESSENT_INTERVIEW.jpeg?v=1732602177&w=1920&h=1080"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-              rel="noreferrer"
-            >
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } =
+            this.props;
+        return (
+            <div className='my-3'>
+                <div className='card'>
+                    <span
+                        class='position-absolute top-0 translate-middle badge rounded-pill bg-danger'
+                        style={{ left: '90%', zIndex: '1' }}
+                    >
+                        {source}
+                    </span>
+                    <img
+                        src={
+                            !imageUrl
+                                ? 'https://image.cnbcfm.com/api/v1/image/108068011-1732601507534-gettyimages-2155830976-SCOTT_BESSENT_INTERVIEW.jpeg?v=1732602177&w=1920&h=1080'
+                                : imageUrl
+                        }
+                        className='card-img-top'
+                        alt='...'
+                    />
+                    <div className='card-body'>
+                        <h5 className='card-title'>{title}</h5>
+                        <p className='card-text'>{description}</p>
+                        <p className='card-text'>
+                            <small>
+                                By {author ? author : 'Unknown'} on{' '}
+                                {new Date(date).toUTCString()}{' '}
+                            </small>
+                        </p>
+                        <a
+                            href={newsUrl}
+                            target='_blank'
+                            className='btn btn-sm btn-dark'
+                            rel='noreferrer'
+                        >
+                            Read More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
